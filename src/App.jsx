0a8c180581d1677ecd7fa42aae7844468746e1c3
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-07-18 14:00:48 +0300

    add profile

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Profile } from 'components/Profile/Profile';
+import user from '../data/user.json';
+// import { Statistics } from 'components/Statistics/Statistics';
+
+export const App = () => {
+  return (
+    <div
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
